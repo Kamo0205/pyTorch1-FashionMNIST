adcd1f03cb88ad339642de23dbe13939d104c080
Kamogelo Matsomela <Kamogelo.G.Matsomela.05@gmail.com>
2022-06-17 21:17:45 +0200

    downloading dataset

--- fashion.py
+++ fashion.py
@@ -3,3 +3,17 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
