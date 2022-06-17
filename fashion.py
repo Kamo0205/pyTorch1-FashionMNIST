f657ba508a859b1cc7e3908c521f242888a612e3
Kamogelo Matsomela <Kamogelo.G.Matsomela.05@gmail.com>
2022-06-17 21:48:15 +0200

    training process

--- fashion.py
+++ fashion.py
@@ -86,3 +86,10 @@ def test(dataloader: DataLoader, model: NeuralNetwork, loss_fn: nn.CrossEntropyL
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+epochs = 5
+for t in range(epochs):
+    print(f"Rpoch {t+1}\n--------------------")
+    train(train_dataloader,model,loss_fn,optimizer)
+    test(test_dataloader, model, loss_fn)
+    print("Done!")
